5c35b0fb11dc441516153ad5bfc0ce576230fc27
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2020-02-14 08:02:23 -0600

    fix: remove a injection for avoid error in compilation test.

--- app/src/main/java/mx/com/ghg/gilinhohero/MainActivity.kt
+++ app/src/main/java/mx/com/ghg/gilinhohero/MainActivity.kt
@@ -7,7 +7,7 @@ import dagger.android.AndroidInjection
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    AndroidInjection.inject(this)
+    // AndroidInjection.inject(this)
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
